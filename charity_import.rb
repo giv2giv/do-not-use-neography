4c11c03407750db70709e7e67eb9c4f09ee5f03f
Michael Blinn <michael.blinn@giv2giv.org>
2013-03-16 23:02:50 -0400

    initial charity importer

---
+++ charity_import.rb
@@ -0,0 +1,80 @@
+require 'rubygems'
+require 'neography' #neo4j access library - https://github.com/maxdemarzi/neography/
+require 'spreadsheet' # excel library - http://spreadsheet.ch
+require 'geokit'
+require 'awesome_print'
+
+include GeoKit::Geocoders
+
+load 'neo4j_config.rb'
+
+
+
+@neo = Neography::Rest.new
+
+#Open the excel file passed in from the commandline
+book = Spreadsheet.open ARGV[0]
+
+#Get the first worksheet
+worksheet = book.worksheet 0
+
+#counter
+ii=0
+
+=begin
+Rows in the excel files:
+[
+    [ 0] "E. I. N.",
+    [ 1] "Name",
+    [ 2] "In Care of Name",
+    [ 3] "Address",
+    [ 4] "City",
+    [ 5] "State",
+    [ 6] "Zip Code",
+    [N] => Foundation Code
+    [ 7] "Group Exemption Number",
+    [ 8] "Subsection Code",
+    [ 9] "Affiliation Code",
+    [10] "Classification Code",
+    [11] "Ruling Date",
+    [12] "Deductibility Code",
+    [13] "Foundation Code",
+    [14] "Activity Code",
+    [15] "Organization Code",
+    [16] "Exempt Organization Status Code",
+    [17] "Advanced Ruling Expiration Date",
+    [18] "Tax Period",
+    [19] "Asset Code",
+    [20] "Income Code",
+    [21] "Filing Requirement Code",
+    [22] "PF Filing Requirement Code",
+    [23] "Blank",
+    [24] "Accounting Period",
+    [25] "Asset Amount",
+    [26] "Income Amount",
+    [27] "Negative Sign",
+    [28] "Form 990 Revenue Amount",
+    [29] "Negative Sign",
+    [30] "NTEE Code",
+    [31] "Sort or Secondary Name"
+]
+=end
+
+# cycle through every row
+worksheet.each do |row|
+
+  if row != nil #&& ii > 0 # skip because first row contains headers
+  	ein = row[0].to_s()
+  	name = row[1].to_s()
+  	address = row[3].to_s()
+  	city = row[4].to_s()
+  	state = row[5].to_s()
+  	zip = row[6].to_s()
+	geodata = MultiGeocoder.geocode(address + city + state + zip)
+	@neo.create_node('type' => 'charity', 'ein' => ein, 'name' => name, 'address'=> address, 'city' => city, 'state' => state, 'zip' => zip, 'latitude' => geodata.lat, 'longitude' => geodata.lng)
+  	ii = ii+1
+	exit if ii > 5
+  end
+
+end
+puts "imported #{ii} charities."
