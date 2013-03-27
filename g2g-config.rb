841acf2b647ec2f4d4d20066dd26074a6881518f
Michael Blinn <michael.blinn@giv2giv.org>
2013-03-26 22:58:57 -0400

    comments, making sponsor index

--- g2g-config.rb
+++ g2g-config.rb
@@ -3,28 +3,42 @@
 Core g2g settings
 =end
 
+# The nonprofit running the fund
+SPONSOR_ORGANIZATION_NAME = "giv2giv"
+
 # The name of the charity index
 CHARITY_INDEX="charity_index"
 # The name of the donor index
 DONOR_INDEX="donor_index"
 # The name of the package index
 PACKAGE_INDEX="package_index"
-=begin END core g2g settings
+# The name of the sponsoring organization index
+SPONSOR_INDEX="sponsor_index"
+
+=begin
+END core g2g settings
 =end
+
+
 
 
 =begin
 Charity import settings
 =end
+
 # Define directory into which to write excel files
 EXCEL_DIRECTORY = "charity_excel_files"
+
 =begin
 END charity import settings
 =end
 
+
+
 =begin
 Dwolla-specific settings
 =end
+
 # Dwolla processes all transactions $10 and under for free
 DWOLLA_FEE_THRESHOLD = 10.00
 
---
+++ sponsor_organization.rb
@@ -0,0 +1,18 @@
+load 'g2g-config.rb'
+
+# Populate neo4j with the sponsoring organization name - should only be done once
+def sponsoring_organization_initial_population()
+
+        sponsoring_organization = Neography::Node.create("name"=>SPONSOR_ORGANIZATION_NAME)
+        sponsoring_organization.add_to_index(SPONSOR_INDEX,"name",SPONSOR_ORGANIZATION_NAME)
+
+end
+
+
+# find the neo4j node for the sponsoring organization
+# returns the neo4j node
+def fetch_sponsoring_organization_node()
+
+	return Neography::Node.find(SPONSOR_INDEX,"name",SPONSOR_ORGANIZATION_NAME)
+
+end
