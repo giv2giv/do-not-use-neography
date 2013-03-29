06394d2a08dae5b209b9588ed2b53e056ed2aabc
Michael Blinn <michael.blinn@giv2giv.org>
2013-03-29 15:56:08 -0400

    oops, sudo service neo4j-service start

--- app.rb
+++ app.rb
@@ -1,12 +1,30 @@
 # app.rb
 require 'rubygems'
-# require 'sinatra/base'
 require 'sinatra'
+require 'json'
+require 'neography'
+
+load 'config/g2g-config.rb'
 
 
 # class App < Sinatra::Base
   set :static, true
   set :public_folder, File.dirname(__FILE__) + '/static'
+
+
+# Example getting charity by EIN 'rackup' go to lynx localhost:9292/charities/611413914
+# Run  sudo service neo4j-service start && rackup &
+# Run lynx localhost:9292/charities/611413914
+# 611413914 is an EIN of a charity. Charities are indexed by EIN as detailed below
+  get "/charities/:ein" do
+	@ein = params[:ein]
+
+	node = Neography::Node.find(CHARITY_EIN_INDEX, "ein", @ein)
+
+	@name = node.name
+	erb :charities
+
+  end
 
   get "/" do
     erb :home_index
