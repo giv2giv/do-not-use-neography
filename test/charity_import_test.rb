3f53d9b9bed168c3402e76c3987d1ff317e98bd0
Dan Funk <dan@ook.(none)>
2013-04-04 20:47:20 -0400

    Adding a really dumb test

---
+++ test/charity_import_test.rb
@@ -0,0 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+require 'test/unit'
+
+#require File.expand_path '../lib/giv2giv/charity_import.rb', __FILE__
+
+class HomepageTest < Test::Unit::TestCase
+
+
+  def test_homepage
+    assert true
+  end
+
+
+end
