b1108252e573d58baa7d9ab0cabfbc89eb5d96c0
Michael Blinn <michael.blinn@giv2giv.org>
2013-04-23 20:42:45 -0400

    Better, cleaner, faster, stronger. Also, find_by_id()

--- models/charity.rb
+++ models/charity.rb
@@ -1,14 +1,14 @@
 
+load 'lib/functions.rb'
+
 class Charity
 
 	attr_accessor :node
 
 	def self.create( ein=nil, name=nil, address=nil, city=nil, state=nil, zip=nil, ntee_common_code=nil, ntee_core_code=nil )
 
-		# Since we're adding to indexes we must use Phase 1
-		@neo4j = Neography::Rest.new
-
-		@node = @neo4j.create_node(
+		@node = Neography::Node.create(
+                        "id" => generate_unique_id(),
 			"ein" => ein,
 			"name" => name,
 			"address" => address,
@@ -19,21 +19,29 @@ class Charity
 			"ntee_core_code" => ntee_core_code
 		)
 
-		# Adding the node to an index via
-		@neo4j.add_node_to_index(CHARITY_NAME_INDEX, CHARITY_NAME_INDEX, name, @node)
- 		@neo4j.add_node_to_index(CHARITY_EIN_INDEX, CHARITY_EIN_INDEX, ein, @node)
- 		@neo4j.add_node_to_index(TYPE_INDEX, TYPE_INDEX, CHARITY_TYPE, @node)
+		# Adding the node to an index 
+		@node.add_to_index(CHARITY_EIN_INDEX, CHARITY_EIN_INDEX, ein)
+		@node.add_to_index(CHARITY_NAME_INDEX, CHARITY_NAME_INDEX, name)
+		@node.add_to_index(ID_INDEX, ID_INDEX, @node.id)
+
+		# Not sure if this works - add node as type "Charity" for easy retrieval of all charities
+		@node.add_to_index(TYPE_INDEX, TYPE_INDEX, CHARITY_TYPE)
 
 	end # initialize
 
 	def self.find_by_ein( ein )
 
-		@neo4j = Neography::Rest.new
-
-		# Set the class variable to the found node(s) & return either the node or nil
-		@node = @neo4j.get_node_index(CHARITY_EIN_INDEX, CHARITY_EIN_INDEX, ein)
+		@node = Neography::Node.find(CHARITY_EIN_INDEX, CHARITY_EIN_INDEX, ein)
+		rescue Neography::NeographyError => err # Don't throw the error
 
 	end # find
+
+	def self.find_by_id( id )
+
+                @node = Neography::Node.find(ID_INDEX, ID_INDEX, id)
+                rescue Neography::NeographyError => err # Don't throw the error
+
+        end # find
 
 	def self.find_by_name( name )
 
@@ -41,18 +49,18 @@ class Charity
 
 		# This is best done using a Cypher query, so it's a little different
 
-		# this is now re-broken
+		# This doesn't work, but *should*
+		Neography::Rest.new.find_node_index(CHARITY_NAME_INDEX, "name:#{name}*").map { |n| n["data"]["name"] }.to_json
+		rescue Neography::NeographyError => err # Don't throw the error
 
-		cypher = "START me=node:#{CHARITY_NAME_INDEX}({query}) 
-            		me.name
-            		ORDER BY me.name
-            		LIMIT 15"
-  			query = "name:*#{name}*"
-  			@neo4j.execute_query(cypher, 
-                    		{:query => query })["data"].map{|x| 
-                           		{ label: x[1], 
-                             		value: x[0] }
-                         	}.to_json   
+
+  		#query = "name:#{name}*"
+		#cypher = "START n=node:#{CHARITY_NAME_INDEX}('#{query}*') return n.name LIMIT 15"
+  			#@neo4j.execute_query(cypher, 
+                    		#{:query => query })["data"].map{|x| 
+                           		#{ label: x[1], 
+                             		#value: x[0] }
+                         	#}.to_json 
 
 	end #  find_name
 
