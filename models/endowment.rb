d03b031bd5db09820f622eb6c5a3c57fb4d84d26
Michael Blinn <michael.blinn@giv2giv.org>
2013-04-23 22:10:49 -0400

    creation and structural relationships - not yet functional

--- models/endowment.rb
+++ models/endowment.rb
@@ -1,90 +1,114 @@
+load 'lib/functions.rb'
+
 class Endowment
 #may not need following line if we dont use datamapper, which makes a certain amount of sense
 #include DataMapper::Resource 
 #has n : donors, required: false
 
-#this is like the charity package of the StautnonLocalPackage, including charities:TiesForHomelessGuys
+#this is like the charity endowment of the StautnonLocalPackage, including charities:TiesForHomelessGuys
 #and SPCA...... also, need better name for this (givdowments? etc)
 
 
 # This code does not work -MPB
 
-def create_endowment(name, amount, frequency)
+	def self.create(owner_id, name, amount, frequency)
 
-        endowment_node = Neography::Node.create(
-                "name" => name,
-                "amount" => amount,  # 5   dollars
-                "frequency" => frequency # per   month
-        )
+        	@node = Neography::Node.create(
+			"id" => generate_unique_id(),
+                	"name" => name,
+                	"amount" => amount,  # 5   dollars
+                	"frequency" => frequency # per   month
+        	)
+	
+        	# Add this node to the endowment indices for easy retrieval
+        	@node.add_to_index(ENDOWMENT_NAME_INDEX, ENDOWMENT_NAME_INDEX, name)
+        	@node.add_to_index(ID_INDEX, ID_INDEX, @node.id)
+	
+        	# Add this node to the type index for easy retrieval of all endowments
+        	@node.add_to_index(TYPE_INDEX, TYPE_INDEX, ENDOWMENT_TYPE)
+	
+        	# Look up the Donor owner's / endowment creator's node by id 
+        	owner_donor = Neography::Node.find(ID_INDEX, ID_INDEX, owner_id)
+	
+        	# Relate the donor to the endowment
+        	owner_donor.outgoing(ENDOWMENT_OWNER_REL) << @node
+	
+	end
 
-        # Add this node to the package name index for easy retrieval using name
-        endowment_node.add_to_index(ENDOWMENT_NAME_INDEX, "name", data["name"])
+	def self.add_investment_fund( endowment_id )
+	
+        	# Look up the (only) Investment fund's node
+		# This will need to change when we have more than one fund
+        	investment_fund_node = Neography::Node.find(TYPE_INDEX, TYPE_INDEX, INVESTMENT_FUND_TYPE)
 
-        # Look up the Donor owner's / endowment creator's node by email
-        owner_donor = Neography::Node.find(DONOR_EMAIL_INDEX, "email", email) # could also use any other indexed value -- maybe access token?
+	
+		# Look up the endowment by function argument
+		@node = Neography::Node.find(ID_INDEX, ID_INDEX, endowment_id)
 
-        # Relate the donor to the endowment
-        owner_donor.outgoing(ENDOWMENT_OWNER_REL) << endowment_node
+        	# Relate the endowment to the investment fund
+        	investment_rel = @node.outgoing(ENDOWMENT_INVESTMENT_REL) << investment_fund_node
+        	investment_rel.percent = 100;
 
+	end
 
-
-        # Look up the Investment fund's node
-        investment_fund_node = Neography::Node.find(TYPE_INDEX, TYPE_INDEX, INVESTMENT_FUND_TYPE) # We need a primary key for investment funds
-
-        # Relate the endowment to the investment fund
-        investment_rel = endowment_node.outgoing(ENDOWMENT_INVESTMENT_REL) << investment_fund_node
-        investment_rel.percent = 100;
-
-
-
-        # Now we need to add a relationship for each charity
-
-        # ... to be continued -MPB
-
-
-end
-
-end
-
+	def self.add_charity( endowment_id, charity_id )
+	
+		# Look up the endowment by function argument
+        	@node = Neography::Node.find(ID_INDEX, ID_INDEX, endowment_id)
+	
+		# Look up the charity by function argument
+        	charity_node = Neography::Node.find(ID_INDEX, ID_INDEX, charity_id)
+	
+		# Relate the endowment *to* the charity
+        	charity_rel = @node.outgoing(ENDOWMENT_GRANTS_REL) << charity_node
+	
+	end
+	
+	
+	def self.add_donor( endowment_id )
+		#add the donor to the package
+	
+		# Look up the endowment by function argument
+        	@node = Neography::Node.find(ID_INDEX, ID_INDEX, endowment_id)
+	
+        	# Look up the donor by session var donor ID
+        	donor_node = Neography::Node.find(ID_INDEX, ID_INDEX, session[:id])
+	
+        	# Relate the donor *to* the endowment
+        	donor_rel = donor_node.outgoing(ENDOWMENT_DONOR_REL) << @node
+	
+	end
+	
 =begin
-property :creator # the donor who created this
-
-def add_donor(donor)
-	#add the donor to the donors
-end
-
 def remove_donor(donor)
 	#remove the donor
 end
 
-def add_fund
-	#add an investment fund
-end
 
 def remove_fund
-	#remove fund from package
+	#remove fund from endowment
 end
 
 
 def add_charity(charity)
-	#needs check for authorized user to change the package/endowment
-	#if authorized, change the package/endowment
+	#needs check for authorized user to change the endowment/endowment
+	#if authorized, change the endowment/endowment
 end
 
 def remove_charity(charity)
-	#check for authorized user to change the package/endowment (i.e., user who created it)
+	#check for authorized user to change the endowment/endowment (i.e., user who created it)
 	#remove the charity
 end
 
 def fork_endowment(forker)
-	# Used when a donor changes a package to which there are multiple subscribers
-	# Best solution: Donors have preference for what to do when the package changes (set upon signup)
+	# Used when a donor changes a endowment to which there are multiple subscribers
+	# Best solution: Donors have preference for what to do when the endowment changes (set upon signup)
 	# “Ask me (Allow changes if no answer)”, “Ask me (no if no answer)”, “Always allow”, “Discard changes”
 # All “Allow” donors participate in the endowment where the changes occur
 # All “Discard” donors participate in the endowment where the changes do not occur
 end
 
 
-
-end
 =end
+
+end # End of class
