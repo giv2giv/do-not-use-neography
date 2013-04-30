14f6b5fe9f46f0a79e7b9dfb16ce85c7d8310b69
Michael Blinn <michael.blinn@giv2giv.org>
2013-04-30 00:11:46 -0400

    Using Rack::Session::Cookie

--- app.rb
+++ app.rb
@@ -7,9 +7,15 @@ require 'bcrypt'
 require 'awesome_print'
 
 load 'config/g2g-config.rb'
+load 'lib/functions.rb'
 
 class App < Sinatra::Application
-  enable :sessions
+
+use Rack::Session::Cookie, :key => 'rack.session',
+                           :domain => SPONSOR_ORGANIZATION_WEBSITE,
+                           :path => '/',
+                           :expire_after => 2592000, # In seconds
+                           :secret => generate_unique_id()
 
   configure :production do
     set :haml, { :ugly=>true }
