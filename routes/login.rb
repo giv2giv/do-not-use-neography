84fab28b02d32f5af3b298d9d40c2d9780636392
Michael Blinn <michael.blinn@giv2giv.org>
2013-04-30 00:10:14 -0400

    Renaming donorsignin

--- routes/login.rb
+++ routes/login.rb
@@ -1,44 +1,16 @@
 class App < Sinatra::Application
-  post "/donorsignup" do
-    # can use a curl command for sending json to this route, using a command line command in the format below
-    # curl --data '{"first_name":"Data","city":"Data","password":"Data","address1":"Data",
-    # "address2":"Data","city":"Data","state":"Data","country":"Data","zip":"Data","node_id":"Data",
-    # "created_at":"Data","facebook_token":"Data","dwolla_token":"Data","twitter_token":"Data"}' 
-    # http://localhost:9393/donorsignup
-    
-    # Post JSON to this endpoint
-    # {"email":"president.whitehouse.gov","password":"somethingfunny"}
-    
-    content_type :json
-    data=JSON.parse(request.body.read)
-    puts "parsing JSON data"
-    puts data
-    puts ""
-    
-    # search to see if the email has been used for another account
-    @donor = Donor.find(data["email"])
-    
-    # if there was nothing found by the email check, proceed to create new donor. otherwise, display the results of the email search
-    if @donor == nil
-      puts "Created user >>>> "
-      @donor=Donor.create(data["first_name"], data["email"], data["password"], data["address1"], data["address2"], data["city"], data["state"], data["country"], data["zip"], data["created_at"], data["facebook_token"], data["dwolla_token"], data["twitter_token"] )
-      return @donor
-    else
-      puts "USER EXISTS >>>> "
-      puts @donor
-      return @donor
-    end
-    
-    #session[:email] = donor_node.email
-    
-    # Return ephemeral id for look-up during development, also name, email -- watch ID iterate
-    #        content_type :json
-    #	{ :id => donor_node.neo_id, :email => donor_node.email, :password => donor_node.password }.to_json
-  end
 
-  post '/donorsignin' do
+#We want to do something like:
+#https://gist.github.com/amscotti/1384524
+
+  post '/login' do
     content_type :json
     data=JSON.parse(request.body.read)
+
+    puts 'hi' + data[:email];
+
+    session[:email] = data["email"]
+    session[:password] = data["password"]
     email = data["email"]
     password = data["password"]
     
