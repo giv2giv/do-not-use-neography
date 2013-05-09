6bbd1772560dd0e12ce14349376f7a50b0ec9f59
Michael Blinn <michael.blinn@giv2giv.org>
2013-05-09 14:08:47 -0400

    fetch_processor_node()

--- lib/functions.rb
+++ lib/functions.rb
@@ -1,9 +1,11 @@
 
 
 def fetch_sponsor_organization_node()
-
         return Neography::Node.find(SPONSOR_INDEX,SPONSOR_INDEX,SPONSOR_ORGANIZATION_NAME)
+end
 
+def fetch_processor_node()
+	return Neography::Node.find(TYPE_INDEX, TYPE_INDEX, PROCESSOR_TYPE)
 end
 
 def generate_unique_id()
